3f779e65befa309630e37110dbd6ae06d571fd51
Yassine Goujdad <goujdad.yass@gmail.com>
2025-07-06 00:31:31 +0100

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './components/Form'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Acceuil from './layers/Acceuil';
+import NewAdminForm from './components/NewAdminForm';
+import EmplyeHome from './layers/EmplyeHome';
+
+function App() {
+  
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route index path="/" element={<Acceuil />}/>
+          <Route path="/LogIn" element={<Form />}/>
+          <Route path="/AjouterAdministrateur" element={<NewAdminForm />}/>
+          <Route path="/avis" element={<EmplyeHome /> }/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
