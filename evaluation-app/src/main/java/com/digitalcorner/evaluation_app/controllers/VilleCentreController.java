b2c3fbe5ce811c977362caa9d29c073e9362e7be
walid-mataich <walidmataich1@gmail.com>
2025-07-21 08:40:41 +0100

    implementing admin dashboard

--- evaluation-app/src/main/java/com/digitalcorner/evaluation_app/controllers/VilleCentreController.java
+++ evaluation-app/src/main/java/com/digitalcorner/evaluation_app/controllers/VilleCentreController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/superadmin/centres")
+@RequestMapping("/adminsuperadmin/centres")
 public class VilleCentreController {
     final VilleCentreService villeCentreService;
 
